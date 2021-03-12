371240e36e45b7ad651d74e522efc0b97e26f7d5
badi34 <58332816+badria-dev-marker@users.noreply.github.com>
2021-03-12 12:28:11 +0100

    Update parfum.js

--- src/main/resources/static/srcmath/js/parfum.js
+++ src/main/resources/static/srcmath/js/parfum.js
@@ -1,6 +1,3 @@
-/**
- * 
- */
 
 
 //parti parfum
@@ -10,10 +7,35 @@
 
 
 
+document.addEventListener('DOMContentLoaded', function () {
+	let timeoutID;
+	let i = 1;
+	
+	function maFunc(){ 
+		if(i < 20) {
+			imgs[i - 1].style.opacity = "0";
+			imgs[i].style.opacity = "1";
+			
+			
+			i = i + 1;
+			
+			
+			 timeoutID = setTimeout(maFunc, 1800);
+		}
+		
+	}
+
+	var imgs =  document.querySelectorAll(".img");
+	
+	maFunc();
 
 
 
 });
+
+
+
+
 
 
 
